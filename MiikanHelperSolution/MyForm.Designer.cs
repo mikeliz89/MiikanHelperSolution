76ba395aa914c2967123f093c7157ac46abba1ce
Miika Kontio <miika.kontio@prime.fi>
2022-07-21 14:42:00 +0300

    Add project files.

---
+++ MiikanHelperSolution/MyForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MiikanHelperSolution {
+  partial class MyForm {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if(disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.inputToOutputButton = new System.Windows.Forms.Button();
+      this.inputTextBox = new System.Windows.Forms.TextBox();
+      this.outputTextBox = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.outputRemoveDuplicatesButton = new System.Windows.Forms.Button();
+      this.resultsTextBox = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.outputRowCountLabel = new System.Windows.Forms.Label();
+      this.outputCombineEveryEachOtherRowButton = new System.Windows.Forms.Button();
+      this.inputCountRowsButton = new System.Windows.Forms.Button();
+      this.outputCountRowsButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // inputToOutputButton
+      // 
+      this.inputToOutputButton.Location = new System.Drawing.Point(195, 181);
+      this.inputToOutputButton.Name = "inputToOutputButton";
+      this.inputToOutputButton.Size = new System.Drawing.Size(154, 23);
+      this.inputToOutputButton.TabIndex = 0;
+      this.inputToOutputButton.Text = "Lisää heittomerkit ja pilkut";
+      this.inputToOutputButton.UseVisualStyleBackColor = true;
+      this.inputToOutputButton.Click += new System.EventHandler(this.inputToOutputButton_Click);
+      // 
+      // inputTextBox
+      // 
+      this.inputTextBox.Location = new System.Drawing.Point(68, 46);
+      this.inputTextBox.Multiline = true;
+      this.inputTextBox.Name = "inputTextBox";
+      this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.inputTextBox.Size = new System.Drawing.Size(380, 120);
+      this.inputTextBox.TabIndex = 1;
+      // 
+      // outputTextBox
+      // 
+      this.outputTextBox.Location = new System.Drawing.Point(555, 46);
+      this.outputTextBox.Multiline = true;
+      this.outputTextBox.Name = "outputTextBox";
+      this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.outputTextBox.Size = new System.Drawing.Size(380, 120);
+      this.outputTextBox.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(65, 24);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(52, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Input lista";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(552, 24);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(60, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Output lista";
+      // 
+      // outputRemoveDuplicatesButton
+      // 
+      this.outputRemoveDuplicatesButton.Location = new System.Drawing.Point(555, 181);
+      this.outputRemoveDuplicatesButton.Name = "outputRemoveDuplicatesButton";
+      this.outputRemoveDuplicatesButton.Size = new System.Drawing.Size(108, 23);
+      this.outputRemoveDuplicatesButton.TabIndex = 5;
+      this.outputRemoveDuplicatesButton.Text = "Poista duplikaatit";
+      this.outputRemoveDuplicatesButton.UseVisualStyleBackColor = true;
+      this.outputRemoveDuplicatesButton.Click += new System.EventHandler(this.outputRemoveDuplicates_Click);
+      // 
+      // resultsTextBox
+      // 
+      this.resultsTextBox.Location = new System.Drawing.Point(1024, 46);
+      this.resultsTextBox.Multiline = true;
+      this.resultsTextBox.Name = "resultsTextBox";
+      this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.resultsTextBox.Size = new System.Drawing.Size(380, 120);
+      this.resultsTextBox.TabIndex = 6;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(1021, 24);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(42, 13);
+      this.label3.TabIndex = 7;
+      this.label3.Text = "Results";
+      // 
+      // outputRowCountLabel
+      // 
+      this.outputRowCountLabel.AutoSize = true;
+      this.outputRowCountLabel.Location = new System.Drawing.Point(900, 24);
+      this.outputRowCountLabel.Name = "outputRowCountLabel";
+      this.outputRowCountLabel.Size = new System.Drawing.Size(0, 13);
+      this.outputRowCountLabel.TabIndex = 8;
+      // 
+      // outputCombineEveryEachOtherRowButton
+      // 
+      this.outputCombineEveryEachOtherRowButton.Location = new System.Drawing.Point(669, 181);
+      this.outputCombineEveryEachOtherRowButton.Name = "outputCombineEveryEachOtherRowButton";
+      this.outputCombineEveryEachOtherRowButton.Size = new System.Drawing.Size(129, 23);
+      this.outputCombineEveryEachOtherRowButton.TabIndex = 9;
+      this.outputCombineEveryEachOtherRowButton.Text = "Yhdistä joka toinen rivi";
+      this.outputCombineEveryEachOtherRowButton.UseVisualStyleBackColor = true;
+      this.outputCombineEveryEachOtherRowButton.Click += new System.EventHandler(this.outputCombineEveryEachRowButton_Click);
+      // 
+      // inputCountRowsButton
+      // 
+      this.inputCountRowsButton.Location = new System.Drawing.Point(355, 181);
+      this.inputCountRowsButton.Name = "inputCountRowsButton";
+      this.inputCountRowsButton.Size = new System.Drawing.Size(93, 23);
+      this.inputCountRowsButton.TabIndex = 10;
+      this.inputCountRowsButton.Text = "Laske rivit";
+      this.inputCountRowsButton.UseVisualStyleBackColor = true;
+      this.inputCountRowsButton.Click += new System.EventHandler(this.inputCountRowsButton_Click);
+      // 
+      // outputCountRowsButton
+      // 
+      this.outputCountRowsButton.Location = new System.Drawing.Point(804, 181);
+      this.outputCountRowsButton.Name = "outputCountRowsButton";
+      this.outputCountRowsButton.Size = new System.Drawing.Size(93, 23);
+      this.outputCountRowsButton.TabIndex = 11;
+      this.outputCountRowsButton.Text = "Laske rivit";
+      this.outputCountRowsButton.UseVisualStyleBackColor = true;
+      this.outputCountRowsButton.Click += new System.EventHandler(this.outputCountRowsButton_Click);
+      // 
+      // MyForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1512, 761);
+      this.Controls.Add(this.outputCountRowsButton);
+      this.Controls.Add(this.inputCountRowsButton);
+      this.Controls.Add(this.outputCombineEveryEachOtherRowButton);
+      this.Controls.Add(this.outputRowCountLabel);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.resultsTextBox);
+      this.Controls.Add(this.outputRemoveDuplicatesButton);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.outputTextBox);
+      this.Controls.Add(this.inputTextBox);
+      this.Controls.Add(this.inputToOutputButton);
+      this.Name = "MyForm";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button inputToOutputButton;
+    private System.Windows.Forms.TextBox inputTextBox;
+    private System.Windows.Forms.TextBox outputTextBox;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button outputRemoveDuplicatesButton;
+    private System.Windows.Forms.TextBox resultsTextBox;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label outputRowCountLabel;
+    private System.Windows.Forms.Button outputCombineEveryEachOtherRowButton;
+    private System.Windows.Forms.Button inputCountRowsButton;
+    private System.Windows.Forms.Button outputCountRowsButton;
+  }
+}
+
---
+++ MiikanHelperSolution/MyForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MiikanHelperSolution {
+  public partial class MyForm : Form {
+    public MyForm() {
+      InitializeComponent();
+    }
+
+    //input to output and add commas and single quotes
+    private void inputToOutputButton_Click(object sender, EventArgs e) {
+      var list = GetRowsAsList(inputTextBox);
+
+      var outputList = new List<string>();
+      for(int i = 0; i < list.Count; i++) {
+        if(i == list.Count - 1) {
+          Console.WriteLine("Last item : " + list[i]);
+          outputList.Add("'" + list[i] + "'");
+        } else {
+          outputList.Add("'" + list[i] + "', ");
+        }
+      }
+      outputTextBox.Text = String.Join("\r\n", outputList);
+      outputRowCountLabel.Text = outputList.Count + "";
+      //results
+      resultsTextBox.Text = $"Muutettu {list.Count} riviä";
+    }
+
+    private List<string> GetRowsAsList(TextBox textBox) {
+      var inputText = textBox.Text;
+      var list = inputText.Split(new[] { '\r', '\n' }).Where(x => !string.IsNullOrEmpty(x)).ToList();
+      return list;
+    }
+
+    //remove duplicates
+    private void outputRemoveDuplicates_Click(object sender, EventArgs e) {
+      var list = GetRowsAsList(outputTextBox);
+      var outputList = list.Distinct().ToList();
+      //output
+      outputTextBox.Text = String.Join("\r\n", outputList);
+      outputRowCountLabel.Text = outputList.Count + "";
+      //results
+      var q = from x in list
+              group x by x into g
+              let count = g.Count()
+              orderby count descending
+              select new { Value = g.Key, Count = count };
+      resultsTextBox.Text = "";
+      foreach(var x in q) {
+        if(x.Count > 1) {
+          resultsTextBox.Text += Environment.NewLine + $" Value {x.Value} Count: {x.Count}";
+        }
+      }
+    }
+
+    //ouput combine every each row 
+    private void outputCombineEveryEachRowButton_Click(object sender, EventArgs e) {
+      var list = GetRowsAsList(outputTextBox);
+
+      var outputList = new List<string>();
+      string value = "";
+      if(list.Count > 1) {
+        for(int i = 1; i < list.Count + 1; i++) {
+          if(i % 2 == 0) {
+            value += list[i - 1];
+            outputList.Add(value);
+          } else { value = list[i - 1] + " "; }
+        }
+      } else if(list.Count == 1) {
+        outputList.Add(list[0]);
+      }
+      //output
+      outputTextBox.Text = String.Join("\r\n", outputList);
+      outputRowCountLabel.Text = outputList.Count + "";
+    }
+
+    private void inputCountRowsButton_Click(object sender, EventArgs e) {
+      //results
+      var list = GetRowsAsList(inputTextBox);
+      resultsTextBox.Text = $"Input list count {list.Count}";
+    }
+
+    private void outputCountRowsButton_Click(object sender, EventArgs e) {
+      //results
+      var list = GetRowsAsList(outputTextBox);
+      resultsTextBox.Text = $"Output list count {list.Count}";
+    }
+  }
+}
